80c6a02279a6e233796d7000a0fffd43dee6fb3b
kalebthe1st <88403660+kalebthe1st@users.noreply.github.com>
2024-01-06 14:21:09 -0800

    finished the nav bar

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -1,19 +1,109 @@
+import { useEffect, useRef } from "react";
+import logo from "../../assets/images/logo.png";
+import userImg from "../../assets/images/avatar-icon.png";
+import { NavLink, Link } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
 
-import React from "react";
-import logo from "../../assets/images/logo.png"
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/bussiness",
+    display: "Find Bussiness",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
 
 const Header = () => {
-    return <header className="header flex items-center">
-        <div className="container">
-            <div className="flex items-center justify-between">
-                {/* {logo ======== place} */}
-                <div>
-                    <h2>Logo</h2>
-                    {/* <img src={logo} alt="" /> */}
-                </div>
-            </div>
-        </div>
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
 
+  const handleStickyHeader = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleStickyHeader();
+    return () => window.removeEventListener("scroll", handleStickyHeader);
+  });
+
+  const toggleMenu = ()=> menuRef.current.classList.toggle('show__menu');
+
+  return (
+    <header className="header flex items-center" ref={headerRef}>
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/* {logo ======== place} */}
+          <div>
+            <h2>Logo</h2>
+            {/* <img src={logo} alt="" /> */}
+          </div>
+
+          {/* +++++++++ menu ++++++ */}
+
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={link.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                        : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
+                    }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* ========= nav Right ++====== */}
+          <div className="flex items-center gap-4">
+            <div className="hidden">
+              <Link to="/">
+                <figure className="w-[35px] h-[45px] rounded-full cursor-pointer">
+                  <img
+                    src={userImg}
+                    className="w-full rounded-full"
+                    alt=""
+                    srcset=""
+                  />
+                </figure>
+              </Link>
+            </div>
+            <Link to="/login">
+              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
+                Login
+              </button>
+            </Link>
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
+          </div>
+        </div>
+      </div>
     </header>
-} 
+  );
+};
 export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
+    return <>
+    {/* +++++++++ hero section ++++++++ */}
+
+    <>
+    <section className="hero__section pt-[60px] 2xl:h-[800px]"></section>
+    </>
+    </>
 } 
 export default Home;
